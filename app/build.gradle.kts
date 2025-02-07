e2644fcb4bc39b34059e3217fcc218af6f2e0907
Rajesh <rkharat98@live.com>
2025-02-07 20:02:04 +0530

    Put menu title and close button in a constrained layout: - For better alignment of title and close button.  - This results in close/open menu to be a the same height. - Feels even better!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.gratus.meditationtrakcer"
         minSdk = 31
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.15.a" // Format: Major (1), Minor (13), Patch (2)
+        versionCode = 15
+        versionName = "1.15.b" // Format: Major (1), Minor (13), Patch (2)
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
